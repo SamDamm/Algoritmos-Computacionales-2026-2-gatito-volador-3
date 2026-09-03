063697f689954d58557e89d53c2e6f8c93188c48
SamDamm <samdamm60@gmail.com>
2026-09-03 11:34:55 -0500

    punto 4

---
+++ Taller 1 algortimos/punto4.py
@@ -0,0 +1,22 @@
+#Taller numero 1 Algoritmos
+#Samuel Ricardo Cala Herrera 2251450
+#Nicolas de Jesus Gonzalez Gamboa 2251867
+#1/09/2026
+#Ejercicio 4
+import math as mt
+n = int(input("Ingrese un numero entero positivo: "))
+
+if n <= 0 :
+    print("El numero debe ser mayor a 0")
+
+numero = n
+resultado = 0
+posicion = 1
+
+while n > 0 :
+    digito = n % 10
+    resultado = resultado + (digito * posicion)
+    posicion = posicion * 100
+    n = n // 10
+
+print(f"El numero {numero} con ceros intercalados es: {resultado} ")
